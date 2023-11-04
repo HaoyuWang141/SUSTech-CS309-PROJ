17b01245b8328c5655fefb558a9ac9ca1eadc55d
haoyu <haoyu.wang141@outlook.com>
2023-11-05 00:59:22 +0800

    [update] deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd springboot/
-mvn clean package -DskipTests
+which mvn
+/home/lighthouse/apache-maven-3.9.4/bin/mvn clean package -DskipTests
 
 cd target/
 nohup java -jar dormitory.jar > ./nohup.out 2>&1 &
