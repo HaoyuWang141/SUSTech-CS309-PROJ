5f49dd29b0630c76f0ddcd25759357dcfc4a887c
YiyuLiu-Rin <rinland@qq.com>
2024-01-05 06:44:39 +0800

    .

--- springboot/src/main/java/com/ooad/dormitory/interceptor/WebMvcConfig.java
+++ springboot/src/main/java/com/ooad/dormitory/interceptor/WebMvcConfig.java
@@ -14,11 +14,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
         return new LoginInterceptor();
     }
 
+    @Bean
+    public RateLimitInterceptor rateLimitInterceptor() {
+        return new RateLimitInterceptor();
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(loginInterceptor()).addPathPatterns("/**")
 //                .excludePathPatterns("/login", "/logout", "/css/**", "/js/**", "/images/**");
         registry.addInterceptor(loginInterceptor()).addPathPatterns("/student/notification/get2WithAuth");
+
+        registry.addInterceptor(rateLimitInterceptor()).addPathPatterns("/**");
 
         // addPathPatterns("/**") 表示拦截所有请求
         // excludePathPatterns("/login", "/logout", "/css/**", "/js/**", "/images/**") 表示排除这些路径
