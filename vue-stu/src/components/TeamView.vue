a71965a8182d298455d2eaa6f83d51c1987345d2
yhChenY <1281807522@qq.com>
2024-01-05 02:50:30 +0800

    TODO

--- vue-stu/src/components/TeamView.vue
+++ vue-stu/src/components/TeamView.vue
@@ -7,14 +7,10 @@ const teammates = ref([]);
 const invitationList = ref([]);
 const inviteDialogVisible = ref(false);
 const invitationListVisible = ref(false);
+const favorListVisible = ref(false)
 const inviteForm = reactive({
     studentID: "",
 });
-
-function removeTeammate(index: number) {
-    teammates.value.splice(index, 1)
-    //TODO
-}
 
 async function invite() {
     try {
@@ -37,17 +33,29 @@ async function invite() {
     }
 }
 
-async function acceptInvitation(index:number) {
-    try{
-
-    }catch (error) {
+async function acceptInvitation(index: number) {
+    try {
+        await axiosInstance.post(
+            "/student/team/accept2",
+            {},
+            {
+                params: {
+                    invitationId: invitationList.value[index]['id'],
+                },
+            }
+        ).then(response => {
+            console.log("TeamView: acceptInvitation() ->")
+            console.log(response)
+        })
+    } catch (error) {
         console.log(error)
     }
 }
+
 async function updateTeamMembers() {
     try {
         await axiosInstance.get(
-            "student/team/getTeam2",
+            "/student/team/getTeam2",
             {
                 params: {
                     studentAccountId: localStorage.getItem("studentId"),
@@ -57,6 +65,7 @@ async function updateTeamMembers() {
             response => {
                 console.log("updateTeamMembers() ->")
                 console.log(response)
+                teammates.value = response.data
             }
         )
     } catch (error) {
@@ -76,6 +85,41 @@ async function getInvitations() {
         ).then(response => {
             console.log("getInvitations() ->")
             console.log(response)
+            invitationList.value = response.data
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function quitTeam() {
+    try {
+        await axiosInstance.post(
+            "/student/team/quitTeam2",
+            localStorage.getItem("studentId"),
+        ).then(response => {
+            console.log("TeamView quitTeam() ->")
+            console.log(response)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function getFavorList() {
+    try {
+        console.log( localStorage.getItem("currentTeam"))
+        await axiosInstance.get(
+            "student/dormitory/getFavoriteList",
+            {
+                params: {
+                    teamId: localStorage.getItem("currentTeam"),
+                },
+            },
+        ).then(response => {
+            console.log("TeamView getFavorList() ->")
+            console.log(response)
+            favorListVisible.value = true
         })
     } catch (error) {
         console.log(error)
@@ -83,7 +127,8 @@ async function getInvitations() {
 }
 
 onMounted(() => {
-
+    updateTeamMembers()
+    getInvitations()
 })
 </script>
 
@@ -104,20 +149,23 @@ onMounted(() => {
       </span>
     </template>
   </el-dialog>
-  <el-dialog title="被邀请记录"
+  <el-dialog v-model="invitationListVisible"
+             title="被邀请记录"
              style="width: 50%; min-width: 300px">
     <el-table style="width: 100%"
               :data="invitationList">
-      <el-table-column prop="name" label="姓名"></el-table-column>
-      <el-table-column prop="student_id" label="学号"></el-table-column>
+      <el-table-column prop="inviter_id" label="学号"></el-table-column>
       <el-table-column fixed="right" label="操作">
         <template #default="scope">
-          <el-button type="primary" @click="">
+          <el-button type="primary" @click="acceptInvitation(scope.$index)">
             接受
           </el-button>
         </template>
       </el-table-column>
     </el-table>
+  </el-dialog>
+  <el-dialog v-model="favorListVisible">
+<!--    TODO-->
   </el-dialog>
   <!--  invisible part end-->
   <el-card class="team-view-card">
@@ -130,42 +178,53 @@ onMounted(() => {
       </div>
     </template>
     <div class="team-view-body">
-      <div style="display: flex; flex-direction: row">
-        <el-button type="primary" @click="inviteDialogVisible = true">
+      <div style=" display: flex; flex-direction: column; width: 20%;">
+        <el-button type="primary"
+                   style="width: 80%;margin: 10px 0 0 12px;"
+                   @click="inviteDialogVisible = true">
           邀请
         </el-button>
-        <el-button type="primary">
+        <el-button type="primary"
+                   style="width: 80%;margin: 10px 0 0 12px;"
+                   @click="getFavorList()">
           查看收藏
         </el-button>
-        <el-button type="primary" @click="getInvitations()">
+        <el-button type="primary"
+                   style="width: 80%;margin: 10px 0 0 12px;"
+                   @click="invitationListVisible = true">
           查看邀请
         </el-button>
+        <el-button type="primary"
+                   style="width: 80%;margin: 10px 0 0 12px;"
+                   @click="quitTeam()">
+          退出队伍
+        </el-button>
       </div>
-      <div>
-        <!--        this div is for displaying teammates-->
-        <el-table :data="teammates" style="width: 100%">
+      <div style="width: 80%;">
+        <!-- this div is for displaying teammates -->
+        <el-table max-height="190px" :data="teammates">
           <el-table-column prop="name" label="姓名"/>
-          <el-table-column prop="sid" label="学号"/>
-          <el-table-column fixed="right" label="操作">
-            <template #default="scope">
-              <el-popconfirm
-                  width="250px"
-                  confirm-button-text="Yes"
-                  cancel-button-text="No"
-                  title="Are you sure to remove him/her?"
-                  @confirm="removeTeammate(scope.$index)"
-                  @cancel=""
-              >
-                <template #reference>
-                  <el-button
-                      type="danger"
-                      size="small">
-                    移除
-                  </el-button>
-                </template>
-              </el-popconfirm>
-            </template>
-          </el-table-column>
+          <el-table-column prop="student_id" label="学号"/>
+          <!--          <el-table-column fixed="right" label="操作">-->
+          <!--            <template #default="scope">-->
+          <!--              <el-popconfirm-->
+          <!--                  width="250px"-->
+          <!--                  confirm-button-text="Yes"-->
+          <!--                  cancel-button-text="No"-->
+          <!--                  title="Are you sure to remove him/her?"-->
+          <!--                  @confirm="removeTeammate(scope.$index)"-->
+          <!--                  @cancel=""-->
+          <!--              >-->
+          <!--                <template #reference>-->
+          <!--                  <el-button-->
+          <!--                      type="danger"-->
+          <!--                      size="small">-->
+          <!--                    移除-->
+          <!--                  </el-button>-->
+          <!--                </template>-->
+          <!--              </el-popconfirm>-->
+          <!--            </template>-->
+          <!--          </el-table-column>-->
         </el-table>
       </div>
     </div>
@@ -187,5 +246,10 @@ onMounted(() => {
     margin-bottom: 5%;
     margin-left: 5%;
     margin-right: 2.5%;
+}
+
+.team-view-body {
+    display: flex;
+    flex-direction: row;
 }
 </style>
