fbbf255752bca86cb147f20567dbd61bed291c99
YiyuLiu-Rin <rinland@qq.com>
2024-01-05 00:55:32 +0800

    .

--- springboot/src/main/java/com/ooad/dormitory/controller/student/ProfileController.java
+++ springboot/src/main/java/com/ooad/dormitory/controller/student/ProfileController.java
@@ -1,9 +1,11 @@
 package com.ooad.dormitory.controller.student;
 
 import com.ooad.dormitory.entity.StudentAccount;
+import com.ooad.dormitory.exception.BackEndException;
 import com.ooad.dormitory.mapper.AuthenticationMapper;
 import com.ooad.dormitory.mapper.StudentAccountMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Time;
@@ -23,43 +25,48 @@ public class ProfileController {
     }
 
     @PostMapping("/set2")
-    public void setProfile2(String studentAccountId,
-                           @RequestParam(required = false) String photoUrl,
-                           @RequestParam(required = false) Time sleepTime,
-                           @RequestParam(required = false) Time wakeUpTime,
-                           @RequestParam(required = false) Integer airConditionerTemperature,
-                           @RequestParam(required = false) Boolean snore,
-                           @RequestParam(required = false) String qq,
-                           @RequestParam(required = false) String email,
-                           @RequestParam(required = false) String wechat) {
-        StudentAccount studentAccount = studentAccountMapper.selectById(studentAccountId);
-        assert studentAccount != null;
+    public ResponseEntity<?> setProfile2(String studentAccountId,
+                                      @RequestParam(required = false) String photoUrl,
+                                      @RequestParam(required = false) Time sleepTime,
+                                      @RequestParam(required = false) Time wakeUpTime,
+                                      @RequestParam(required = false) Integer airConditionerTemperature,
+                                      @RequestParam(required = false) Boolean snore,
+                                      @RequestParam(required = false) String qq,
+                                      @RequestParam(required = false) String email,
+                                      @RequestParam(required = false) String wechat) {
+        try {
+            StudentAccount studentAccount = studentAccountMapper.selectById(studentAccountId);
+            assert studentAccount != null;
 
-        if (photoUrl != null) {
-            studentAccount.setPhotoUrl(photoUrl);
-        }
-        if (sleepTime != null) {
+            if (photoUrl != null) {
+                studentAccount.setPhotoUrl(photoUrl);
+            }
+            if (sleepTime != null) {
 //            studentAccount.setSleepTime(sleepTime);
-        }
-        if (wakeUpTime != null) {
+            }
+            if (wakeUpTime != null) {
 //            studentAccount.setWakeUpTime(wakeUpTime);
+            }
+            if (airConditionerTemperature != null) {
+                studentAccount.setAirConditionerTemperature(airConditionerTemperature);
+            }
+            if (snore != null) {
+                studentAccount.setSnore(snore);
+            }
+            if (qq != null) {
+                studentAccount.setQq(qq);
+            }
+            if (email != null) {
+                studentAccount.setEmail(email);
+            }
+            if (wechat != null) {
+                studentAccount.setWechat(wechat);
+            }
+            studentAccountMapper.updateById(studentAccount);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            throw new BackEndException("set profile failed!");
         }
-        if (airConditionerTemperature != null) {
-            studentAccount.setAirConditionerTemperature(airConditionerTemperature);
-        }
-        if (snore != null) {
-            studentAccount.setSnore(snore);
-        }
-        if (qq != null) {
-            studentAccount.setQq(qq);
-        }
-        if (email != null) {
-            studentAccount.setEmail(email);
-        }
-        if (wechat != null) {
-            studentAccount.setWechat(wechat);
-        }
-        studentAccountMapper.updateById(studentAccount);
     }
 
 
